b8cafce3630e8fc74c63aa89fd5d2bec6d5789b3
Guga Cavalieri <gugacavalieri@gmail.com>
2019-01-09 09:59:28 -0200

    adding uncss and husky to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,13 @@ module.exports = function (grunt) {
           dest: 'dist/img'
         }]
       }
+    },
+    uncss: {
+      dist: {
+        files: {
+          'dist/css/tidy.css': ['index.html']
+        }
+      }
     }
   });
 
@@ -51,7 +58,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-uncss');
 
-  grunt.registerTask('default', ['concat', 'cssmin', 'uglify', 'imagemin']);
+  grunt.registerTask('default', ['concat', 'cssmin', 'uglify', 'imagemin', 'uncss']);
 
 };
