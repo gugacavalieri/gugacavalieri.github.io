35fdd687f6395f84987dd4e932409a44a0d5ffbe
Guga Cavalieri <gugacavalieri@gmail.com>
2024-07-19 17:01:06 -0600

    Update Resume, Profile Pic, Add Info

--- src/components/About.js
+++ src/components/About.js
@@ -4,23 +4,19 @@ export default class About extends Component {
     let resumeData = this.props.resumeData;
     return (
       <section id="about">
-         <div className="row">
-
-            <div className="three columns">
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
-            </div>
-
-            <div className="nine columns main-col">
-
-               <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-               
-            </div>
-         </div>
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+          </div>
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>
+              {
+                resumeData.aboutme
+              }
+            </p>
+          </div>
+        </div>
       </section>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,50 +6,50 @@ export default class Header extends Component {
     let resumeData = this.props.resumeData;
     return (
       <React.Fragment>
-      
-      <header id="home">
-         <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-            <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-               <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#footer">Contact</a></li>
-            </ul>
-         </nav>
 
-         <div className="row banner">
+        <header id="home">
+          <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <ul id="nav" className="nav">
+              <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+              <li><a className="smoothscroll" href="#about">About</a></li>
+              <li><a className="smoothscroll" href="#resume">Resume</a></li>
+              <li><a className="smoothscroll" href="#footer">Contact</a></li>
+            </ul>
+          </nav>
+
+          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}</h1>
-               <h3>{resumeData.role}</h3>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{resumeData.roleDescription}
-               </h3>
-               <hr/>
-               <ul className="social">
-                  {
-                    resumeData.socialLinks && resumeData.socialLinks.map(item =>{
-                      return(
-                              <li key={item.name}>
-                                <a href={item.url} target="_blank">
-                                  <FontAwesomeIcon icon={['fab', item.iconClass]} />
-                                  </a>
-                              </li>
-                            )
-                          }
+              <h1 className="responsive-headline">I am {resumeData.name}</h1>
+              <h3>{resumeData.role}</h3>
+              <h3 style={{ color: '#fff', fontFamily: 'sans-serif ' }}>{resumeData.roleDescription}
+              </h3>
+              <hr />
+              <ul className="social">
+                {
+                  resumeData.socialLinks && resumeData.socialLinks.map(item => {
+                    return (
+                      <li key={item.name}>
+                        <a href={item.url} target="_blank">
+                          <FontAwesomeIcon icon={['fab', item.iconClass]} />
+                        </a>
+                      </li>
                     )
                   }
-               </ul>
+                  )
+                }
+              </ul>
             </div>
-         </div>
+          </div>
 
-         <p className="scrolldown">
+          <p className="scrolldown">
             <a className="smoothscroll" href="#about">
               <FontAwesomeIcon icon={['fas', 'chevron-circle-down']} />
             </a>
-         </p>
+          </p>
 
-      </header>
+        </header>
       </React.Fragment>
     );
   }
